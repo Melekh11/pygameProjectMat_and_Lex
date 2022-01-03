d17b5cd05da79488e6106b139518403782dbda1a
Матвей <cktkutxd@gmail.com>
2022-01-03 22:53:25 +0300

    add heroes without moving

--- classes.py
+++ classes.py
@@ -1,7 +1,42 @@
 import pygame
 import pytmx
+import sys
+import os
 
+COUNT_SHIP = 1
+tile_w, tile_h = 40, 40
+board_w, board_h = 25, 20
+size = w, h = tile_w * board_w, tile_h * board_h
+pygame.init()
+game_screen = pygame.display.set_mode(size)
+pygame.display.set_caption('A little farm')
 MAPS_DIR = "map"
+PIC_DIR = "pictures"
+cell_size = 40
+
+animals = pygame.sprite.Group()
+ships = pygame.sprite.Group()
+wolfs = pygame.sprite.Group()
+dogs = pygame.sprite.Group()
+cowboys = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join(PIC_DIR, name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
 
 class Fild:
     def __init__(self, filename):
@@ -19,7 +54,126 @@ class Fild:
                     if tile:
                         screen.blit(tile, (x*self.tile_size, y*self.tile_size))
 
-    # def make_map(self):
-    #     surface = pygame.Surface((self.w, self.h))
-    #     self.render(surface)
-    #     return surface
+
+class It(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, image_name, *group,  hp=1, count=1):
+        super().__init__(*group)
+        self.image = load_image(image_name)
+        self.rect = self.image.get_rect()
+        self.rect.x = x*cell_size
+        self.rect.y = y*cell_size
+        self.hp = hp
+        self.count = count
+
+    def update(self, event, move=40):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.rect = self.rect.move(-move, 0)
+            elif event.key == pygame.K_RIGHT:
+                self.rect = self.rect.move(move, 0)
+            elif event.key == pygame.K_UP:
+                self.rect = self.rect.move(0, -move)
+            elif event.key == pygame.K_DOWN:
+                self.rect = self.rect.move(0, move)
+
+    # def update(self, event, move=cell_size):
+    #     pass
+        # if self.count > 0:
+        #     self.count -= 1
+        #     up = False
+        #     down = False
+        #     left = False
+        #     right = False
+        #     if event.type == pygame.KEYDOWN:
+        #         if event.key == pygame.K_LEFT:
+        #             left = True
+        #         if event.key == pygame.K_RIGHT:
+        #             right = True
+        #         if event.key == pygame.K_UP:
+        #             up = True
+        #         if event.key == pygame.K_DOWN:
+        #             down = True
+        #         if up == down:
+        #             up = False
+        #             down = False
+        #         if left == right:
+        #             left = False
+        #             right = False
+        #         move = (0, 0)
+
+
+class Ship(It):
+    def __init__(self, x, y, *group, image_name="ship.png"):
+        super().__init__(x, y, image_name, *group)
+        self.add(animals)
+
+class Wolf(It):
+    def __init__(self, x, y, *group, image_name="wolf.png"):
+        super().__init__(x, y, image_name, *group)
+        self.add(animals)
+
+
+class Dog(It):
+    def __init__(self, x, y, *group, image_name="dog.png"):
+        super().__init__(x, y, image_name, *group, count=2)
+        self.add(animals)
+
+class Cowboy(It):
+    def __init__(self, x, y, *group, image_name="cowboy.png"):
+        super().__init__(x, y, image_name, *group, count=3)
+
+    # def update(self, event, move=cell_size):
+    #     if self.count > 0 and event.type == pygame.KEYDOWN:
+    #         self.count -= 1
+    #         up = False
+    #         down = False
+    #         left = False
+    #         right = False
+    #
+    #         if event.key == pygame.K_LEFT:
+    #             left = True
+    #         if event.key == pygame.K_RIGHT:
+    #             right = True
+    #         if event.key == pygame.K_UP:
+    #             up = True
+    #         if event.key == pygame.K_DOWN:
+    #             down = True
+    #         if up == down:
+    #             up = False
+    #             down = False
+    #         if left == right:
+    #             left = False
+    #             right = False
+    #         move = [0, 0]
+    #         if left:
+    #             move[0] = -1
+    #         elif right:
+    #             move[0] = 1
+    #         if up:
+    #             move[1] = -1
+    #         elif down:
+    #             move[1] = 1
+    #         self.rect = self.rect.move(move[0]*cell_size, move[1]*cell_size)
+
+
+
+
+
+
+
+class Fild:
+    def __init__(self, filename):
+        self.map = pytmx.load_pygame(f"{MAPS_DIR}/{filename}", )
+        self.h = self.map.height
+        self.w = self.map.width
+        self.tile_size = self.map.tilewidth
+
+    def render(self, screen):
+        ti = self.map.get_tile_image_by_gid
+        for layer in self.map.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer):
+                for x, y, gid in layer:
+                    tile = ti(gid)
+                    if tile:
+                        screen.blit(tile, (x*self.tile_size, y*self.tile_size))
--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import pygame
 import pytmx
+import os
+import sys
 from classes import *
 
 tile_w, tile_h = 40, 40
 board_w, board_h = 25, 20
 size = w, h = tile_w * board_w, tile_h * board_h
+pygame.init()
+game_screen = pygame.display.set_mode(size)
+pygame.display.set_caption('A little farm')
 
-FPS = 15
+
+MAPS_DIR = "map"
+PIC_DIR = "pictures"
+cell_size = 40
 
 
-# map = pytmx.load_pygame(f"{MAPS_DIR}/")
 def main():
-    pygame.init()
-    game_screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('A little farm')
     fild = Fild("final_fild.tmx")
     clock = pygame.time.Clock()
     running = True
+    fps = 30
+    ship = Ship(6, 17, ships)
+    wolf = Wolf(3, 3, wolfs)
+    dog = Dog(15, 15, dogs)
+    cowboy = Cowboy(6, 6, cowboys)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         fild.render(game_screen)
+        animals.draw(game_screen)
+        cowboys.draw(game_screen)
+        animals.update(event)
+        cowboys.update(event)
+        clock.tick(fps)
         pygame.display.flip()
     pygame.quit()
 
