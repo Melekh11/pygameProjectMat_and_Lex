5310595f131fd76d845c9bca9d13a3d12f614c37
Матвей <cktkutxd@gmail.com>
2022-01-04 16:23:57 +0300

    spam animals from "heroes.txt"

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import os
 import sys
 from classes import *
 
+
+def load_level(filename):
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, 'n'), level_map))
+
+
+
 tile_w, tile_h = 40, 40
 board_w, board_h = 25, 20
 size = w, h = tile_w * board_w, tile_h * board_h
@@ -22,15 +31,26 @@ def main():
     clock = pygame.time.Clock()
     running = True
     fps = 30
-    ship = Ship(6, 17, ships)
-    wolf = Wolf(3, 3, wolfs)
-    dog = Dog(15, 15, dogs)
-    cowboy = Cowboy(6, 6, cowboys)
+    # ship = Ship(6, 17, ships)
+    # wolf = Wolf(3, 3, wolfs)
+    # dog = Dog(15, 15, dogs)
+    # cowboy = Cowboy(6, 6, cowboys)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         fild.render(game_screen)
+        animals_map = load_level("heroes.txt")
+        for i in range(len(animals_map)):
+            for j in range(len(animals_map[i])):
+                if animals_map[i][j] == "s":
+                    Ship(j, i, ships)
+                elif animals_map[i][j] == "w":
+                    Wolf(j, i, wolfs)
+                elif animals_map[i][j] == "d":
+                    Dog(j, i, dogs)
+                elif animals_map[i][j] == "c":
+                    Cowboy(j, i, cowboys)
         animals.draw(game_screen)
         cowboys.draw(game_screen)
         animals.update(event)
